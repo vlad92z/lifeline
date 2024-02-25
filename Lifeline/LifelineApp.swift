60dc4c27a41add94c6e1f3a23e0b97835930df0f
Vlad <vlad92z@users.noreply.github.com>
2024-02-25 09:13:41 +0000

    Empty SwiftUI iOS project

---
+++ Lifeline/LifelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifelineApp.swift
+//  Lifeline
+//
+//  Created by Vlad Z on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LifelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
