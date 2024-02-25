8efb76ec483a5a29f41795d92dece818c917dc4c
Vlad <vlad92z@users.noreply.github.com>
2024-02-25 20:16:26 +0000

    MVP where the user can select their birthday and get a life progress bar

--- Lifeline/ContentView.swift
+++ Lifeline/ContentView.swift
@@ -8,17 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var birthday = Date()
+    let lifeExpectency = 83
+    var age: Double {
+        let now = Date()
+        let ageComponents = Calendar.current.dateComponents(
+            [.year, .month, .day],
+            from: birthday,
+            to: now
+        )
+        
+        let years = Double(ageComponents.year ?? 0)
+        let months = Double(ageComponents.month ?? 0) / 12
+        let days = Double(ageComponents.day ?? 0) / 365.25
+        
+        return years + months + days
+    }
+    
+    var timeLeft: (yearsLeft: Int, weeksLeft: Int, percentageLeft: Int) {
+        let yearsLeft = Int(lifeExpectency - Int(age))
+        let weeksLeft = yearsLeft * 52
+        let percentage = 100 * yearsLeft / lifeExpectency
+        return (yearsLeft, weeksLeft, percentage)
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                VStack {
+                    Text("Lifeline")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.top, 10)
+                        .padding(.bottom, 20)
+                    let timeLeft = timeLeft
+                    Text("You have \(timeLeft.yearsLeft) years, " +
+                         "\(timeLeft.weeksLeft) weeks or " +
+                         "\(timeLeft.percentageLeft)% left")
+                    ProgressView(remaining: age / Double(lifeExpectency))
+                        .frame(height: 4)
+                    Divider()
+
+                    Text("Your birthday").padding(.top, 40)
+                    DatePicker("Birthday", selection: $birthday, displayedComponents: .date)
+                        .datePickerStyle(WheelDatePickerStyle())
+                        .labelsHidden()
+
+                    Text("Your age is \(String(format: "%.2f", age)) years")
+                        .padding()
+                }
+            }
+
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ Lifeline/ProgressView.swift
@@ -0,0 +1,39 @@
+//
+//  ProgressView.swift
+//  Lifeline
+//
+//  Created by Vlad Z on 25/02/2024.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    
+    let remaining: Double
+    
+    var progress: CGFloat {
+        // Ensure value is between 0 and 1
+        return CGFloat(min(max(self.remaining, 0.0), 1.0))
+    }
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                let width = geometry.size.width
+                let height = geometry.size.height
+                Capsule()
+                    .fill(Color.gray)
+                    .frame(width: width, height: height)
+                Capsule()
+                    .fill(Color.pink)
+                    .frame(width: progress * width, height: height)
+            }
+        }
+    }
+}
+
+#Preview {
+    ProgressView(remaining: 0.7)
+        .frame(height: 4)
+        .previewLayout(.sizeThatFits)
+}
